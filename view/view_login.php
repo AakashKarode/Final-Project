9564ed9ff113a8caf8319cd3476e1dedd134a959
AakashKarode <ark56@njit.edu>
2017-05-06 00:16:48 -0400

    Added View for login

---
+++ view/view_login.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+  <title>Login Page</title>
+  <style type="text/css">
+    div.container {
+      border: 3px solid #01f1f0;
+      padding: 6px;
+      width:300px;
+      margin:20px auto;
+      text-align:center; 
+    }
+    .login {
+      background:#f9f9f9;
+    }
+    .login div {
+      border:2px solid #fff;
+      padding:3px;
+    }
+    .register {
+      background:#f9f9f9;
+    }
+    .register div {
+      border:2px solid #fff;
+      padding:3px;
+    }
+    input[type=text], input[type=password] {
+      padding: 6px 15px;
+      margin: 8px 0;
+      border: 1px solid #ccc;
+    }
+    button {
+      background-color: #4CAF50;
+      color: white;
+      padding: 6px 15px;
+      margin: 8px 0;
+      width: 50%;
+    }
+ 
+  </style>
+</head>
+ </html>
---
+++ view/view_reg.php
@@ -0,0 +1,37 @@
+<html>
+<style>
+
+#body-color{ background-color:#cceeff; }
+		#Sign-Up{
+			background-image:url('img.jpg'); 
+			background-size:400px 300px; 
+			background-repeat:no-repeat; 
+			background-attachment:fixed; 
+			background-position:center; 
+			margin-top:auto; 
+			margin-bottom:auto;
+			margin-right:auto; 
+			margin-left:auto;
+			padding:9px 10px; } 
+			
+			#button{
+				
+				border-radius:10px; 
+				width:100px; 
+				height:40px;
+				background:#00cc99;
+				font-weight:bold;
+				font-size:15px; }
+				
+			table	{
+						 border-collapse: collapse;
+			}
+
+			table, th, td {
+						border: 1px solid black;
+			}
+			input{
+						white-space: nowrap;
+			}
+</style>
+</html>
