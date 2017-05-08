1b0f153095ab075d51a2820fd0eb551333ead0e6
AakashKarode <ark56@njit.edu>
2017-05-07 23:09:09 -0400

    changes made to register page

--- register.php
+++ register.php
@@ -1,53 +1,46 @@
 <?php
-include_once("view/view_reg.php");
+    if(isset($_SESSION['message']))
+    {
+         echo "<div id='error_msg'>".$_SESSION['message']."</div>";
+         unset($_SESSION['message']);
+    }
 ?>
+<!DOCTYPE html>
 <html>
-<head><title>Regitration</title>
+<head>
+  <title>Notes</title>
+  <link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
-	
-		<form>
-		<h1> Registration Page</h1>
-		<form action="home_page.php">
-			<body id="body-color" align="center"> 
-			<div id="Sign-Up"> 
-			<fieldset style="width:97%">
-			<legend>Registration Form</legend> 
-			<table border="1" align="center" length="50%" width="50%">
-			<tr> <form method="POST" action="connectivity-sign-up.php">
-					<td width="auto">Name</td>
-					<td> <input type="text" name="name"></td> 
-			</tr> 
-			<tr> <td>First Name</td>
-					<td width="70% "> <input type="text" name="fname"></td>
-			</tr>
-			<tr> <td>Last Name</td>
-					<td width="70% "> <input type="text" name="lname"></td>
-			</tr>
-			<tr> <td>Date of Birth</td>
-					<td width="70% "> <input type="date" name="dob"></td>
-			</tr>
-			<tr> <td>Gender</td>
-					<td width="70% "> <input type="text" name="gender"></td>
-			</tr>
-			<tr> <td>Email</td>
-					<td> <input type="text" name="email"></td>
-			</tr> 
-			<tr> <td>User Name</td>
-					<td width="70% "> <input type="text" name="user"></td>
-			</tr> 
-			
-			<tr> <td>Password</td>
-					<td> <input type="password" name="pass"></td> 
-			</tr>
-			
-			<tr> <td colspan=2 align="center"><input id="button" type="submit" name="submit" value="Sign-Up"></td>
-					
-			</tr> 
-			</form>
-			</table>
-			</fieldset>
-			</div> 
-</body> 
-</html>
+<div class="header">
+    <h1>Notes-to do list</h1>
+</div>
 
+<form method="post" action="controller.php">
+<input type="hidden" name="postaction" value="reg_user"/><br>
+  <table>
+     <tr>
+           <td>Username : </td>
+           <td><input type="text" name="username" class="textInput"></td>
+     </tr>
+     <tr>
+           <td>Email : </td>
+           <td><input type="email" name="email" class="textInput"></td>
+     </tr>
+      <tr>
+           <td>Password : </td>
+           <td><input type="password" name="password" class="textInput"></td>
+     </tr>
+      <tr>
+           <td>Password again: </td>
+           <td><input type="password" name="password2" class="textInput"></td>
+     </tr>
+      <tr>
+           <td></td>
+           <td><input type="submit" name="register_btn" class="Register"></td>
+     </tr>
+ 
+</table>
+</form>
+</body>
+</html>
