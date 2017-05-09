245963d34182d2f2674888627c381c32d9347d05
AakashKarode <ark56@njit.edu>
2017-05-08 22:46:38 -0400

    changes made in the home page

--- home.php
+++ home.php
@@ -1,32 +1,123 @@
 <?php
-session_start();
 
+require('dbconnect.php');
+/*  $userid = $_GET['userid'];
+  $fname = $_GET['fname'];
+  $lname = $_GET['lname'];
+  require('db.php');
+  require('runq.php');
+  $results = runQuery($sql);
+*/?>
 
-$db=mysqli_connect("localhost","root","","authentication");
-
-
-?>
-<!DOCTYPE html>
 <html>
 <head>
-  <title>Notes</title>
-  <link rel="stylesheet" type="text/css" href="css/style.css"/>
+<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-<div class="header">
-    <h1>Notes-to do list</h1>
+//<hr size="100">
+ // <?php
+  //echo "<div style='text-align:right'><h2>Welcome ".$_COOKIE['login']." !!</h2></div>";
+  //?><hr>
+
+
+<div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h4>My Tasks</h4>
+          <div class="table-responsive">
+            <table id="mytable" class="table table-bordred table-striped" style="color:black;">
+                <thead>
+                  <th style="width:65%">Task</th>
+                  <th style="width:10%">Due Date</th>
+                  <th style="width:10%">Due Time</th>
+                  <th style="width:10%"></th>
+                  <th style="width:10%"></th>
+                  <th style="width:10%"></th>
+                </thead>
+                <table>
+                   <?php foreach($results as $res):?>
+                    <tr>                    
+                      <?php $status = $res['done'];?>
+                       <?php if($status == "No"): ?>
+                      
+                        <td style="width:65%"> <?php echo $res['taskdesc'];?></td>
+                        <td style="width:10%"> <?php echo $res['taskdate'];?></td>
+                        <td style="width:10%"> <?php echo $res['tasktime'];?></td>
+                        <td style="width:10%">
+                          <form action = 'index.php' method = 'post' >
+                            <input type="hidden" class="btn btn-danger" name = 'action' value="edit"/>
+                            <input type="hidden" class="btn btn-danger" name = 'itemid' value="<?php echo $res['id']; ?>"/>
+                            <input type="submit" class="btn btn-danger" value="edit"/>
+                          </form>
+                        </td>
+                        <td style="width:10%">
+                          <form action = 'index.php' method = 'post' >
+                            <input type="hidden" class="btn btn-danger" name = 'action' value="delete"/>
+                            <input type="hidden" class="btn btn-danger" name = 'itemid' value="<?php echo $res['id'];?>"/>
+                            <input type="submit" class="btn btn-danger" value="delete"/>
+                          </form>
+                        </td>
+                        <td style="width:10%">
+                          <form action = 'index.php' method = 'post' >
+                            <input type="hidden" class="btn btn-danger" name = 'action' value="complete"/>
+                            <input type="hidden" class="btn btn-danger" name = 'itemid' value="<?php echo $res['id'];?>"/>
+                            <input type="submit" class="btn btn-danger" value="Complete"/>
+                          </form>
+                        </td>
+                      <?php endif; ?>
+                    </tr>
+                  <?php endforeach;?>
+                </table>
+            </table>
+            
+            <table id="mytable" class="table table-bordred table-striped" style="color:black;">
+                <thead>
+                  <th style="width:65%">Task</th>
+                  <th style="width:10%">Due Date</th>
+                  <th style="width:10%">Due Time</th>
+                  <th style="width:10%"></th>
+                  <th style="width:10%"></th>
+                  <th style="width:10%"></th>
+                </thead>
+                <table>
+                   <?php foreach($results as $res):?>
+                    <tr>                    
+                      <?php $item_status = $res['done'];?>
+                       <?php if($item_status == "Yes"): ?>
+                      
+                        <td style="width:65%"> <?php echo $res['taskdesc'];?></td>
+                        <td style="width:10%"> <?php echo $res['taskdate'];?></td>
+                          <td style="width:10%"> <?php echo $res['tasktime'];?></td>
+                        <td style="width:10%">
+                          <form action = 'index.php' method = 'post' >
+                            <input type="hidden" class="btn btn-danger" name = 'action' value="edit"/>
+                            <input type="hidden" class="btn btn-danger" name = 'item_id' value="<?php echo $res['id']; ?>"/>
+                            <input type="submit" class="btn btn-danger" value="edit"/>
+                          </form>
+                        </td>
+                        <td style="width:10%">
+                          <form action = 'index.php' method = 'post' >
+                            <input type="hidden" class="btn btn-danger" name = 'action' value="delete"/>
+                            <input type="hidden" class="btn btn-danger" name = 'item_id' value="<?php echo $res['id'];?>"/>
+                            <input type="submit" class="btn btn-danger" value="delete"/>
+                          </form>
+                        </td>
+                        
+                      <?php endif; ?>
+                    </tr>
+                  <?php endforeach;?>
+                </table>
+            </table>
+            
+            <form action="addtask.php">
+              <input type="submit" value="Add New Task" class="btn btn-default" />
+            </form>
+          </div>
+      </div>
+    </div>
+  </div>
 </div>
-<?php
-    if(isset($_SESSION['message']))
-    {
-         echo "<div id='error_msg'>".$_SESSION['message']."</div>";
-         unset($_SESSION['message']);
-    }
-?>
-<h1>Home</h1>
-<div>
-    <h4>Welcome <?php echo $_SESSION['username']; ?></h4></div>
-</div>
-<a href="logout.php">Log Out</a>
+<br><hr><br>
+<a href = "comtask.php">Completed task</a>
 </body>
 </html>
